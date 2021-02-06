d6c9f9b55fba592daf55c19b72ed1a85be5aa31c
Alex <alexbrunoalves2011@gmail.com>
2021-02-06 11:02:32 -0500

    created clearRect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#foo";
+ctx.clearRect(0, 0, 800, 600);
+
+ctx.fillStyle = '#f00';
 ctx.fillRect(20, 20, 100, 100);
+
+ctx.fillStyle = '#00f';
+ctx.fillRect(340, 260, 40, 50);
+
